2dc2b013ab8e9f9ca7f849a89b13085feb8cbd4e
khaled <khaled.b7373@hotmail.com>
2014-09-11 22:23:45 +0430

    left

---
+++ My Leitner/src/main/java/ir/khaled/myleitner/helper/Errors.java
@@ -0,0 +1,23 @@
+package ir.khaled.myleitner.helper;
+
+/**
+ * Created by kh.bakhtiari on 5/30/2014.
+ */
+public class Errors {
+    public static final int INVALID_REQUEST_OBJECT = 1;
+    public static final int UNKNOWN_DEVICE = 2;
+    public static final int NO_SUCH_METHOD = 3;
+    public static final int SQL_DEVICE = 4;
+    public static final int MISSING_PARAM = 5;
+    public static final int EXCEPTION_ADD_CARD = 5;
+    public static final int SQL_ERROR_LAST_CARDS = 6;
+    public static final int USER_DOESNT_EXIST = 7;
+    public static final int WRONG_USERNAME_PASSWORD = 8;
+    public static final int EXCEPTION_LOGIN_USER = 9;
+    public static final int REGISTER_WEAK_PASSWORD = 16;
+    public static final int REGISTER_EMAIL_ALREADY_EXISTS = 17;
+    public static final int EXCEPTION_REGISTER_USER = 18;
+    public static final int EXCEPTION_CREATE_LEITNER = 19;
+    public static final int LEITNER_ALREADY_EXISTS = 20;
+    public static final int USER_IS_NOT_LOGGED_IN = 21;
+}
